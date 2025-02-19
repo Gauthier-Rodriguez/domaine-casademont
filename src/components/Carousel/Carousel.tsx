e435716c10b36dd8330f98fd5ed40570ed27716a
Gauthier-Rodriguez <gauthierrodriguez@gmail.com>
2025-02-20 00:23:46 +0100

    add nav to carousel modal

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -34,8 +34,8 @@ const Carousel: React.FC<PropType> = (props) => {
     setSelectedImage(null);
     setIsOpenModal(false)
   };
-  const handleClickOutside = (event : MouseEvent) => {
-    if(modalRef.current && !modalRef.current.contains(event.target as Node)){
+  const handleClickOutside = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
       handelCloseModal();
     }
   }
@@ -73,19 +73,55 @@ const Carousel: React.FC<PropType> = (props) => {
       {selectedImage !== null && (
         <div className="fixed inset-0 bg-black/60 backdrop-blur-sm z-50 flex items-center justify-center">
           <div className="relative max-w-4xl w-full mx-4" ref={modalRef}>
-            
+
             <button
               onClick={handelCloseModal}
               className="absolute -top-12 right-0 p-2 text-white hover:bg-white/20 rounded-full w-auto h-auto transition-colors"
             >
-             x
+              x
             </button>
             <img
               src={`/pictures/${selectedImage + 1}.jpg`}
               alt={getImageAlt(selectedImage)}
-              className="w-full h-auto rounded-lg"
+              className="w-full h-auto rounded-lg object-cover"
             />
+
           </div>
+          <div ref={modalRef}>
+            {selectedImage > 0 ? (
+              <div
+                className="absolute cursor-pointer bottom-1/2 left-5 text-white z-50"
+                onClick={() => setSelectedImage(selectedImage - 1)}
+              >
+                Prev
+              </div>
+            ) : (
+              <div
+                className="absolute cursor-pointer bottom-1/2 left-5 text-white z-50"
+                onClick={() => setSelectedImage(14)}
+              >
+                Prev
+              </div>
+            )}
+
+            {selectedImage < 14 ? (
+              <div
+                className="absolute cursor-pointer bottom-1/2 right-5 text-white z-50"
+                onClick={() => setSelectedImage(selectedImage + 1)}
+              >
+                Next
+              </div>
+            ) : (
+              <div
+                className="absolute cursor-pointer bottom-1/2 right-5 text-white z-50"
+                onClick={() => setSelectedImage(0)}
+              >
+                Next
+              </div>
+            )}
+          </div>
+
+
         </div>
       )}
     </>
